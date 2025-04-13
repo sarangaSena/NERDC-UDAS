1ffb91ec08430bdab6ce28cdd323cc77e462ddae
Saranga Senarathna <saranganirwana@gmail.com>
2025-04-13 21:55:05 +0530

    Add files via upload

---
+++ SCADA_Final_Code/SCADA_Final_Code/Debug/SCADA_Final_Code.cpp.d
@@ -0,0 +1,30 @@
+C:\Users\Mechatronic\AppData\Local\Temp\VMBuilds\SCADA_Final_Code\mega_atmega2560\Debug\SCADA_Final_Code.cpp.o: \
+ C:\Users\Mechatronic\AppData\Local\Temp\VMBuilds\SCADA_Final_Code\mega_atmega2560\Debug\SCADA_Final_Code.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Users\Mechatronic\Documents\Arduino\libraries\MCP3424-master/MCP3424.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Ethernet\src/Ethernet.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Ethernet\src/EthernetClient.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Client.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Ethernet\src/EthernetServer.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Server.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Ethernet\src/Dhcp.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Ethernet\src/EthernetUdp.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Udp.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Mudbus/Mudbus.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Enterprise\Common7\IDE\Extensions\pbd1usl1.2ll\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
